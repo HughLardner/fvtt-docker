ed6b939e53f9669db0d47bdef4df676313e2f6d2
HughLardner <hughlardner@gmail.com>
2020-10-19 22:30:21 +0100

    Update run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -1,9 +1,3 @@
 #!/bin/sh 
-DIRECTORY="/opt/foundryvtt"
-if [ ! -d "$DIRECTORY" ]; then
-  cd /opt/foundryvtt
-  cp /host/foundryvtt*.zip .
-  unzip -o foundryvtt*.zip
-  rm foundryvtt*.zip
-fi
+
 node resources/app/main.js --dataPath=/data/foundryvtt
