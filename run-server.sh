a88d71b7645f7b7b411abd1ceeed11ca59c9ed98
HughLardner <hughlardner@gmail.com>
2020-10-19 21:44:24 +0100

    Update run-server.sh
    Only copy application if it does not exist

--- run-server.sh
+++ run-server.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-cd /opt/foundryvtt
-cp /host/foundryvtt*.zip .
-unzip -o foundryvtt*.zip
-rm foundryvtt*.zip
+if [! -d "/opt/foundryvtt" ]
+  cd /opt/foundryvtt
+  cp /host/foundryvtt*.zip .
+  unzip -o foundryvtt*.zip
+  rm foundryvtt*.zip
+fi
 node resources/app/main.js --dataPath=/data/foundryvtt
